32bb0dbe6aece8950a58fbaabe2154a0e2152e31
Griffin Hughes <Griffinhughes2020@gmail.com>
2021-12-14 17:57:11 -0500

    commit tree f6326a6c6b73415e3dc9f2a619c4f9e704815760

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+// const returnFirstTwoDrivers = (array) => {
+//     return array.slice(0,2);
+// };
+
+// const returnLastTwoDrivers = (array) => {
+
+//     return array.splice(-2);
+// };
+
+
+
+
+
+
+function returnFirstTwoDrivers(array){
+    return array.slice(0,2);
+
+}
+
+
+function returnLastTwoDrivers(array){
+ return array.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function(value) {
+        return multiplier*value;
+    };
+
+};
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers=(array,selectionType) =>
+    {return selectionType(array)}
